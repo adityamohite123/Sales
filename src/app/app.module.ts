74aff2d5a82bc581d8958d786099d199ce610cae
Aditya M <adityamohite196@gmail.com>
2020-12-08 17:13:17 +0530

    Implemented sale and new product modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NewProductComponent } from './welcome/new-product/new-product.component';
+import { SalesComponent } from './welcome/sales/sales.component';
+import { CacheService } from './shared/cache.service';
+import { ProductSearchPipe } from './shared/product-search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    NewProductComponent,
+    SalesComponent,
+    ProductSearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
-  providers: [],
+  exports: [
+    ProductSearchPipe
+  ],
+  providers: [CacheService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
